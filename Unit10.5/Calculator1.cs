89046b42fb43d0ef93ebcb9d61c7caa3dfe9408c
rodion3000 <rodion.papizh@mail.ru>
2022-06-07 16:04:58 +0500

    Добавьте файлы проекта.

---
+++ Unit10.5/Calculator1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unit10._5
+{
+    public class Calculator1: MiniCalculator
+    {
+        ILogger Logger { get; }
+        public Calculator1(ILogger logger)
+        {
+            Logger = logger;
+        }
+
+        public void Calc()
+        {
+            Logger.Event("Калькулятор начал свою работу");
+            Console.WriteLine("Введите первое число для сложения");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число для сложения");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{x}+{y} равно {x + y}");
+        }
+    }
+}
---
+++ Unit10.5/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+namespace Unit10._5
+{
+    class Program
+    {
+        static ILogger Logger { get; set; }
+        static void Main()
+        {
+            Logger = new Logger();
+            var calculator1 = new Calculator1(Logger);
+            int x;
+            int y;
+            try
+            {
+                calculator1.Calc();
+            }
+            catch (Exception ex)
+            { Console.WriteLine(ex.Message); }
+        }
+    }
+    public interface MiniCalculator
+    {
+        void Calc();
+    }
+    public interface ILogger
+    {
+        void Event(string message);
+        void Error(string mesaage);
+
+    }
+    public class Calculator : MiniCalculator
+    {
+        public void Calc()
+        {
+            Console.WriteLine("Введите первое число для сложения");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число для сложения");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine($"{x}+{y} равно {x + y}");
+        }
+
+
+    }
+    public class Logger : ILogger
+    {
+        public void Error(string mesaage)
+        {
+            Console.WriteLine(mesaage);
+        }
+
+        public void Event(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
+
